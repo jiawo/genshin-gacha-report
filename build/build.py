f7c9431eac64a42151a924356f4fe434b03f7997
hexianglong <736626079@qq.com>
2021-02-28 01:39:58 +0800

    修改bug

--- build/build.py
+++ build/build.py
@@ -7,7 +7,7 @@ CURR_PATH = os.path.dirname(os.path.abspath(__file__))
 target_path = os.path.join(CURR_PATH, "..", "src")
 os.chdir(target_path)
 
-cmd = "pyinstaller -F app.py -n gacha-report%s.exe" VERSION
+cmd = "pyinstaller -F app.py -n gacha-report%s.exe" % VERSION
 os.system(cmd)
 
 os.chdir(CURR_PATH)
