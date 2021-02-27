7dab908e0b0ff435320d1238482d777173a33451
hexianglong <736626079@qq.com>
2021-02-28 01:39:23 +0800

    添加版本号

--- build/build.py
+++ build/build.py
@@ -1,12 +1,13 @@
 import os
 
 
+VERSION = "v1.0.0"
 CURR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 target_path = os.path.join(CURR_PATH, "..", "src")
 os.chdir(target_path)
 
-cmd = "pyinstaller -F app.py"
+cmd = "pyinstaller -F app.py -n gacha-report%s.exe" VERSION
 os.system(cmd)
 
 os.chdir(CURR_PATH)
